24eaf9092b53ebd04b09bd528d762c292a99effb
wcfung14 <44503876+wcfung14@users.noreply.github.com>
2022-09-23 10:53:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ import sys
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
-fasta = "./Trigoniulus_corallinus_hic.gff3.longest-gene.proteins.fa"
+fasta_file = "./Trigoniulus_corallinus_hic.gff3.longest-gene.proteins.fa"
 
 pattern_file = "./DEG_gene_id.txt"
 
 output_counter = 0
 pattern_counter = 0
 with open("./output.txt", "w") as output:
-    with open(pattern_file, "r") as pattern_file:
-        for pattern in pattern_file:
+    with open(pattern_file, "r") as pf:
+        for pattern in pf:
             pattern_counter += 1
             print(pattern.strip())
-            with open(fasta, "r") as file:
-                for line in file:
+            with open(fasta_file, "r") as ff:
+                for line in ff:
                     if re.search(pattern.strip(), line):
                         print("yes")
                         output_counter += 1
                         #print(line)
                         #print(next(file))
                         output.write(line)
-                        output.write(next(file))
+                        output.write(next(ff))
         print(output_counter)
         print(pattern_counter)
         assert pattern_counter == output_counter

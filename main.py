e9333196fb2c3b1ba9fb0141b861cc1f9a5245d2
wcfung14 <44503876+wcfung14@users.noreply.github.com>
2022-09-23 10:32:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# https://stackoverflow.com/questions/1767513/how-to-read-first-n-lines-of-a-file
+# https://stackoverflow.com/questions/36027227/grep-for-a-word-and-if-found-print-10-lines-before-and-10-lines-after-the-patte
+
+import re
+import os
+import sys
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+fasta = "./Trigoniulus_corallinus_hic.gff3.longest-gene.proteins.fa"
+
+pattern_file = "./DEG_gene_id.txt"
+
+output_counter = 0
+pattern_counter = 0
+with open("./output.txt", "w") as output:
+    with open(pattern_file, "r") as pattern_file:
+        for pattern in pattern_file:
+            pattern_counter += 1
+            print(pattern.strip())
+            with open(fasta, "r") as file:
+                for line in file:
+                    if re.search(pattern.strip(), line):
+                        print("yes")
+                        output_counter += 1
+                        #print(line)
+                        #print(next(file))
+                        output.write(line)
+                        output.write(next(file))
+        print(output_counter)
+        print(pattern_counter)
+        assert pattern_counter == output_counter
